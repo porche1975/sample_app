aa9118874b20ad7c17b444d1aee4e110ec049f86
Joe Porcheddu <porche1975@yahoo.com>
2012-12-13 15:00:38 -0600

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
+  before_save { self.email.downcase! }
+  before_save :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -21,5 +24,10 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  before_save { self.email.downcase! }
+  
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
