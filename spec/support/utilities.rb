9e29974f4124ed610a132d593ad82949f3df7b09
Joe Porcheddu <porche1975@yahoo.com>
2012-12-14 20:01:35 -0600

    Finish user edit, update, index and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -17,6 +17,15 @@ def find_example_user
   User.find_by_email('user@example.com')
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
